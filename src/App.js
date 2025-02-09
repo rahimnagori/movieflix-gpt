e89dd2d4e96960d608d8cc546708bf61746af0f6
rahim <rahim.nagori@gmail.com>
2025-02-09 23:26:51 +0530

    Configuring Tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to React</h1>
+    <div className="text-center">
+      <h1 className="bg-black text-white text-6xl">Welcome to React</h1>
     </div>
   );
 }
